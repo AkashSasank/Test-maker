e65a3dd7c79408b6d9a837caf37467348b42d471
akashc <akashc@qburst.com>
2020-12-29 10:55:20 +0530

    Set templates dirs.

--- testmaker_backend/settings.py
+++ testmaker_backend/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'testmaker_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'staticfiles')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
